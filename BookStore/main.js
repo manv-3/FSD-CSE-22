33e3a2c6bfe678e569078c4456434fa56ea26e05
Manvendra Singh <mvs35790@gmail.com>
2026-08-19 04:56:31 +0530

    fixed

--- BookStore/main.js
+++ BookStore/main.js
@@ -1,13 +1,13 @@
-function book(){
+function book(bookData){
 const div=document.createElement("div");
 div.setAttribute("class","book");
 const img=document.createElement("img");
-img.setAttribute("src","https://m.media-amazon.com/images/I/81t9cbIICUL.jpg");
+img.setAttribute("src",bookData.img || "https://m.media-amazon.com/images/I/81t9cbIICUL.jpg");
 img.setAttribute("height","200px");
 img.setAttribute("width","200px");
 
 const h2=document.createElement("h2");
-h2.innerText="Price:"+dataPrice+"/-";
+h2.innerText="Price:"+bookData.price+"/-";
 h2.style.color="blue";
 
 
@@ -16,13 +16,8 @@ bt.innerText="add to cart";
 div.appendChild(img);
 div.appendChild(h2);
 div.appendChild(bt);
+return div;
 }
-bookdata.map((b)=>(
-book(b))
-const parent=document.getElementById("book")
-parent.appendChild(div);
-
-
 
 const bookdata=[
     {
@@ -37,5 +32,9 @@ const bookdata=[
     {
         img:"",price:665
     }
-]
+];
+
+const parent=document.getElementById("books");
+const books=bookdata.map((bookData)=>book(bookData));
+parent.append(...books);
 
